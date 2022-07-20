b85175e6d38cf3bef76df56a152191d2028a1025
tigercoin-world <shsfse@DESKTOP-SBS7AAA.localdomain>
2022-07-20 16:42:47 +0900

    update

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
-require('dotenv').config()
+require("dotenv").config();
+const axios = require("axios").default;
 
-const TelegramBot = require('node-telegram-bot-api');
-
-// replace the value below with the Telegram token you receive from @BotFather
+const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
   const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+  bot.sendMessage(chatId, response);
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
+bot.on("message", (msg) => {
+  var btc = "btc";
+  if (msg.text.toString().toLowerCase().indexOf(btc) === 0) {
+    axios
+      .get("https://api.upbit.com/v1/ticker?markets=KRW-BTC")
+      .then(function (response) {
+        const resp = response.data[0].trade_price;
+        const price = resp.toLocaleString() + "원";
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+        bot.sendMessage(msg.chat.id, price); //bot에 보내기
+        console.log(price);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {});
+  }
+
+  var bye = "eth";
+  if (msg.text.toString().toLowerCase().includes(bye)) {
+    axios
+      .get("https://api.upbit.com/v1/ticker?markets=KRW-ETH")
+      .then(function (response) {
+        const resp = response.data[0].trade_price;
+        const price = resp.toLocaleString() + "원";
+
+        bot.sendMessage(msg.chat.id, price); //bot에 보내기
+        console.log(price);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {});
+  }
 });
